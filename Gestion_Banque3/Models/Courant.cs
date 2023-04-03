ed99828f205323256b2692438fd33c3730352a3d
Denys Desmecht <d.desmecht@stag.technofuturtic.education>
2023-04-03 17:10:31 +0200

    MAJ

---
+++ Gestion_Banque3/Models/Courant.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Banque3.Models
+{
+    public class Courant
+    {
+
+        #region Attributs
+
+        public string _Numero;
+        public double _Solde; // on peut passer par un decimal (reccommandé)
+        private double _LignedeCredit; // on peut passer par un decimal (reccommandé)
+        private Personne _Titulaire;
+
+        #endregion
+
+        #region Props
+
+        public string Numero
+        {
+            get { return _Numero; }
+            set { _Numero = value; }
+        }
+
+        public double Solde
+        {
+            get { return _Solde; }
+            set { _Solde = value; }
+        }
+
+        public double LignedeCredit
+        {
+            get { return _LignedeCredit; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Ligne de Crédit ne peut être négative");
+                    return;
+                }
+                else
+                    _LignedeCredit = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get { return _Titulaire; }
+            set { _Titulaire = value; }
+        }
+        #endregion
+
+        #region Methodes
+
+
+        /// <summary>
+        /// (Ma petite méthode) retire un montant au solde
+        /// </summary>
+        /// <param name="montant"> el famoso montant, comme dit Yves</param>
+
+        public void Retrait(double montant)
+        {
+            if (montant <= 0)
+            {
+                Console.WriteLine("montant trop faible");
+                return;
+            }
+            else if (Solde - montant <= -LignedeCredit)
+            {
+                Console.WriteLine("Solde insuffisant");
+                return;
+            }
+            else
+            {
+                Solde -= montant;
+            }
+        }
+
+        /// <summary>
+        /// (ma 2ème petite methode) ajoute un montant au solde 
+        /// </summary>
+        /// <param name="montant"></param>
+
+        public void Depot(double montant)
+        {
+            if (montant <= 0)
+            {
+                Console.WriteLine("Montant trop faible pour un retrait");
+                return;
+
+            }
+            _Solde += montant;
+        }
+
+        public static double operator +(double solde, Courant c)
+        {
+            //if (c.Solde < 0)
+            //{
+            //    return solde + 0.0;
+            //}
+
+            //else
+            //{
+            //    return solde + c.Solde;
+            //}
+
+            //ternaire
+            return solde + ((c.Solde < 0.0) ? 0.0 : c.Solde);
+
+        }
+
+
+
+        #endregion
+
+    }
+}
---
+++ Gestion_Banque3/Program.cs
@@ -0,0 +1,22 @@
+﻿#region Exo Surcharge opérateurs
+
+// Exo 1.1
+//Surcharger l’opérateur « + » de la classe « Courant » afin qu’il retourne la somme, de type double, des soldes.
+//Cependant, les soldes négatifs ne doivent pas être pris en compte.
+//Ajouter une méthode « AvoirDesComptes » à la classe « Banque » recevant en paramètre le titulaire (Personne) qui calculera les avoirs de tous
+//ses comptes en utilisant l’opérateur « + ».
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
