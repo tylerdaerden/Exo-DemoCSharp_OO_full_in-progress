c8767073eb879005512549a9dbd73b182d70df79
Tyler Daerden <daerdentyler@gmail.com>
2023-05-12 10:45:58 +0200

    MAJ

---
+++ Gestion_Banque8/Interfaces/IBanker.cs
@@ -0,0 +1,20 @@
+﻿using Gestion_Banque8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Banque8.Interfaces
+{
+    internal interface IBanker : ICustomer
+    {
+        Personne Titulaire { get; }
+
+        string Numero { get; }
+
+        void AppliquerInteret();
+
+
+    }
+}
---
+++ Gestion_Banque8/Interfaces/ICustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Banque8.Interfaces
+{
+    public interface ICustomer
+    {
+        double Solde { get; }
+        
+        void Depot(double montant);
+
+        void Retrait (double montant);
+
+    }
+}
